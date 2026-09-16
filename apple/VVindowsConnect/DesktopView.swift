044a52bba6ff3007dc749fc0a1b2881499523541
Peter Wang <peterwangsc@gmail.com>
2026-09-16 14:50:47 -0700

    Drop the orphaned font modifier left by the latency readout removal

--- apple/VVindowsConnect/DesktopView.swift
+++ apple/VVindowsConnect/DesktopView.swift
@@ -73,7 +73,6 @@ struct DesktopView: View {
                     default: ProgressView()
                     }
                 }
-                    .font(.caption.monospaced())
             }
             .padding(8)
             .glassBackgroundEffect()
